2ecd3dcc341784daa8e9e701e4ae959b91945e44
KalinskiyAlexander <kalinskiyalexander@github.com>
2022-04-03 16:14:52 +0300

    delete: uuid library

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, {Request, Response} from 'express'
 import bodyParser from 'body-parser';
 import cors from 'cors';
-
+import path from 'path'
+import favicon from 'serve-favicon'
 import {router} from "./router";
 
 const app = express();
@@ -11,6 +12,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded())
 app.use('/hs_01/api', router);
+app.use(favicon(path.join(__dirname, 'images', 'favicon.ico')))
+
 app.get('/', (req: Request, res: Response ) => {
   res.send('<h1>Bloggers API-01</h1>')
 })
